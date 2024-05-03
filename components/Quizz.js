47366476cb19254dbd0f819b06819b65661dd508
GanaF4ll <ganafall9498@gmail.com>
2024-05-03 11:23:44 +0200

    feat: Update Quizz component to fetch and display questions from API

--- components/Quizz.js
+++ components/Quizz.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 import Answer from "./Answer";
 import he from "he";
 
@@ -8,75 +8,76 @@ import axios from "axios";
 
 const Quizz = ({ route }) => {
   const { selectedCategoryId, selectedDifficulty } = route.params;
-  const [questionData, setQuestionData] = useState(null);
+  const [questions, setQuestions] = useState([]);
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
   const [selectedAnswer, setSelectedAnswer] = useState(null);
   const [score, setScore] = useState(0);
 
-  let url = "https://opentdb.com/api.php?amount=10";
-
-  if (selectedCategoryId !== null) {
-    url = url + "&category=" + selectedCategoryId;
-  }
-
-  if (selectedDifficulty === "Easy") {
-    url = url + "&difficulty=easy";
-  } else if (selectedDifficulty === "Medium") {
-    url = url + "&difficulty=medium";
-  } else if (selectedDifficulty === "Hard") {
-    url = url + "&difficulty=hard";
-  }
-
-  const fetchQuizz = async () => {
-    try {
-      const response = await axios.get(url);
-      return response.data.results[0];
-    } catch (error) {
-      if (error.response && error.response.status === 429) {
-        console.log("Too many requests. Please try again later.");
-      } else {
-        console.log("error: ", error);
-      }
-    }
-  };
-
   useEffect(() => {
-    fetchQuizz().then((data) => setQuestionData(data));
-  }, []);
+    const fetchQuizz = async () => {
+      let url = "https://opentdb.com/api.php?amount=10";
 
-  if (!questionData) {
-    return <Text>Loading...</Text>;
-  }
+      if (selectedCategoryId !== null) {
+        url = url + "&category=" + selectedCategoryId;
+      }
 
-  const answers = [
-    ...questionData.incorrect_answers.map((answer) => ({
-      text: answer,
-      value: 0,
-    })),
-    { text: questionData.correct_answer, value: 1 },
-  ];
+      if (selectedDifficulty === "Easy") {
+        url = url + "&difficulty=easy";
+      } else if (selectedDifficulty === "Medium") {
+        url = url + "&difficulty=medium";
+      } else if (selectedDifficulty === "Hard") {
+        url = url + "&difficulty=hard";
+      }
 
-  const shuffledAnswers = answers.sort(() => Math.random() - 0.5);
+      try {
+        const response = await axios.get(url);
+        setQuestions(response.data.results);
+      } catch (error) {
+        if (error.response && error.response.status === 429) {
+          console.log("Too many requests. Please try again later.");
+        } else {
+          console.log("error: ", error);
+        }
+      }
+    };
+
+    fetchQuizz();
+  }, [selectedCategoryId, selectedDifficulty]);
 
   const handleAnswerSelection = (index) => {
     setSelectedAnswer(index);
   };
 
-  const correctAnswerIndex = shuffledAnswers.findIndex(
-    (answer) => answer.value === 1
-  );
-
   const handleValidation = () => {
     if (selectedAnswer === 1) {
-      // Augmenter le score
       setScore(score + 1);
     }
+
+    setCurrentQuestionIndex(currentQuestionIndex + 1);
+
+    setSelectedAnswer(null);
   };
-  const isAnswerSelected = selectedAnswer !== null;
+
+  if (questions.length === 0) {
+    return <Text>Loading...</Text>;
+  }
+
+  const currentQuestion = questions[currentQuestionIndex];
+
+  const answers = [
+    ...currentQuestion.incorrect_answers.map((answer) => ({
+      text: answer,
+      value: 0,
+    })),
+    { text: currentQuestion.correct_answer, value: 1 },
+  ];
+
+  const shuffledAnswers = answers.sort(() => Math.random() - 0.5);
 
   return (
     <View style={style.container}>
       <Text style={style.title}>Score: {score}</Text>
-      <Text style={style.title}>{he.decode(questionData.question)}</Text>
+      <Text style={style.title}>{he.decode(currentQuestion.question)}</Text>
 
       <Answer
         answers={shuffledAnswers}
